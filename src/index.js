9f507a00b8cde3767efad7945f42d9343efa8485
yahya alabdali <d.yaahya@gmail.com>
2023-11-23 14:00:34 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css'
+import '@fortawesome/fontawesome-free/js/all.min';
+
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').map(item => new bootstrap.Tooltip(item))
+
