8d562e4f45b700048cd1e13584bd608bcd5d22f9
d-yaahya <143932713+d-yaahya@users.noreply.github.com>
2025-06-15 00:39:56 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -11,9 +11,28 @@ document.querySelectorAll('.add-to-cart-btn').forEach(item => {
     })
 } )
 
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change' , () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change' , () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+
+
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة" + new Date().getFullYear();
 // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 // const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 
 console.log("أهلا بكم في متجر عربي")
